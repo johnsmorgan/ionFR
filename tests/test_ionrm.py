939a21de9b69f2ed488cc8d540622da1fe20dd68
Alec Thomson (S&A, Kensington WA) <alec.thomson@csiro.au>
2024-09-05 10:59:56 +0800

    test stub

---
+++ tests/test_ionrm.py
@@ -0,0 +1,15 @@
+# import pytest
+# import importlib.resources as resources
+# from ionfr.ionFRM import compute_rm
+
+# TODO: Implement test_compute_rm when we modularise
+# def test_compute_rm():
+
+#     # original test CLI
+#     # ionFRM.py 08h37m05.6s+06d10m14.5s 52d54m54.6sn 6d52m11.7se 2011-10-20T00:00:00 codg2930.11i
+#     rm_results = compute_rm(
+#         rawLatitude="08h37m05.6s",
+#         rawLongitude="+06d10m14.5s",
+#         rawDTime="2011-10-20T00:00:00",
+#         nameIONEX="codg2930.11i",
+#     )
